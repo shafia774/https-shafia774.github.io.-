3d4d619e39057472139876a16d4ba885c16c8532
Shafi Asharaf <84091684+shafia774@users.noreply.github.com>
2021-07-29 12:49:48 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SEVAFARM</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="sevafarm.css">
+</head>
+<body>
+    <div class="first">
+        <a href="index.php"><div><h1 id="sevafarm">SEVAFARM</h1></div></a>
+				<b><img src="sevalogo.png"height="80px" width="120px"><h2>WWW.SEVAFARM.GOV</h2></b>
+   <div class="front">
+         <div class="pic2">
+            <img src="newfarm.jpg" height="400px" width="1200px">
+         </div>
+    </div>
+    <table>
+	     <tr>
+		    <td>
+			     <a href="../sevafarm/notifications/Notifications.php"><img src="Notifications.jpg" height="200px" width="200px" alt="There definetly seems to be a problem here"><h3>Notifications</h3></a>
+		    </td>
+	        <td>
+	    	     <a href="../sevafarm/item entry/item entry.php"><img src="item entry.jpg" height="200px" width="200px" alt="There definetly seems to be a problem here"><h3>Item Entry</h3></a>
+	        </td>
+	         <td>
+	    	     <a href="../sevafarm/Database/Database.php"><img src="Database.jpg" height="200px" width="200px" alt="There definetly seems to be a problem here"><h3>Database</h3></a>
+	         </td>
+        </tr>
+        <tr>
+        	<td>
+                 <a href="../sevafarm/Stocks/Stocks.php"><img src="Stocks.jpg" height="200px" width="200px" alt="There definetly seems to be a problem here"><h3>Stocks</h3></a>
+            </td>
+            <td>
+	             <a href="../sevafarm/Accounts/Accounts.php"><img src="Accounts.jpg" height="200px" width="200px" alt="There definetly seems to be a problem here"><h3>To Pay</h3></a>
+	        </td>
+        </tr>
+    </table>
+    <div class="seasonal">
+        <a href="../sevafarm/seasonal/seasonal.php"><h1>SEASONAL PRODUCTS</h1></a>
+    </div>
+    </div>
+    <div class="storage">
+         <a href="storage/storage.php"><button type="button" class="login">Storage conditions</button>
+         <a href="storage/storage.php"><iframe src="storage/storage.php" height="500" width="100%">Storage conditions</iframe></a>
+    <div>
+    <a href="logout.php"><button type="button" class="login">Log out</button></a>
+</body>
+</html>
